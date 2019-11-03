947d78151ea8d70e9ae9ee3961898ba858a58a9e
Piotr Merski <merskip@gmail.com>
2019-11-03 10:59:32 +0100

    Coloring AST dump

--- Printer/ASTPrinterOutput.cpp
+++ Printer/ASTPrinterOutput.cpp
@@ -44,17 +44,17 @@ std::string ASTPrinterOutput::getOutput() {
 }
 
 std::string ASTPrinterOutput::getTagString(const std::string &tag) {
-    return "[" + tag + "]";
+    return "[\x1B[34m" + tag + "\033[0m]";
 }
 
 std::string ASTPrinterOutput::getChildTagString(const std::string &tag) {
-    return "{" + tag + "}";
+    return "{\x1B[32m" + tag + "\033[0m}";
 }
 
 std::string ASTPrinterOutput::getAttributesString(const std::map<std::string, std::string> &attributes) {
     std::stringstream result;
     for (auto it = attributes.begin(); it != attributes.end(); it++) {
-        result << it->first << "=" << it->second;
+        result << it->first << "=" << "\x1B[33m" << it->second << "\033[0m";
         if (std::next(it) != attributes.end())
             result << ", ";
     }
