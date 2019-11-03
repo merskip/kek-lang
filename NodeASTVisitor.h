bb1cad0c1be43045c2565814c2006600f8b73a1b
Piotr Merski <merskip@gmail.com>
2019-11-03 07:58:21 +0100

    Adding NodeASTVisitor

--- NodeASTVisitor.h
+++ NodeASTVisitor.h
@@ -6,37 +6,23 @@
 
 
 class BinaryOperatorNodeAST;
-
 class CallNodeAST;
-
 class FileNodeAST;
-
 class FunctionBodyNodeAST;
-
 class FunctionPrototypeNodeAST;
-
 class FunctionDefinitionNodeAST;
-
 class NumberNodeAST;
-
 class ReferenceNodeAST;
 
 class NodeASTVisitor {
 
 public:
     virtual void visitBinaryOperatorNode(BinaryOperatorNodeAST *binaryOperatorNode) = 0;
-
     virtual void visitCallNode(CallNodeAST *callNode) = 0;
-
     virtual void visitFileNode(FileNodeAST *fileNode) = 0;
-
     virtual void visitFunctionBodyNode(FunctionBodyNodeAST *functionBodyNode) = 0;
-
     virtual void visitFunctionPrototypeNode(FunctionPrototypeNodeAST *functionPrototypeNode) = 0;
-
     virtual void visitFunctionDefinitionNode(FunctionDefinitionNodeAST *functionDefinitionNode) = 0;
-
     virtual void visitNumberNode(NumberNodeAST *numberNode) = 0;
-
     virtual void visitReferenceNode(ReferenceNodeAST *referenceNode) = 0;
 };
